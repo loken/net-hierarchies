68a214461eca3827c9d3c7f2b715781fd8f544b7
Gaute Løken <gautelo@gmail.com>
2023-04-04 23:00:29 +0200

    Added code doc for Relation.

--- Hierarchies/Relation.cs
+++ Hierarchies/Relation.cs
@@ -1,12 +1,30 @@
 namespace Loken.Hierarchies;
 
+/// <summary>
+/// A <see cref="Parent"/>-<see cref="Child"/> relationship describes the
+/// identifiers that are connected.
+/// </summary>
+/// <typeparam name="TId">The type of identifier.</typeparam>
+/// <remarks>
+/// A <see cref="Relation{TId}"/> is a class so that it can be more easily stored in a database.
+/// Some databases don't accept structs.
+/// </remarks>
 public class Relation<TId>
 	where TId : notnull
 {
+	/// <summary>
+	/// The identifier of the parent.
+	/// </summary>
 	public required TId Parent { get; init; }
 
+	/// <summary>
+	/// The identifier of the child.
+	/// </summary>
 	public required TId Child { get; init; }
 
+	/// <summary>
+	/// The implicit tuple-to-relation operator allow us to tersely instantiate relations.
+	/// </summary>
 	public static implicit operator Relation<TId>((TId parent, TId child) tuple)
 	{
 		return new Relation<TId>
@@ -16,6 +34,11 @@ public class Relation<TId>
 		};
 	}
 
+	/// <summary>
+	/// The implicit relation-to-tuple operator allow us to tersely unwrap a relation
+	/// into a memory efficient tuple, which is a struct, unlike the <see cref="Relation{TId}"/>.
+	/// </summary>
+	/// <param name="relation"></param>
 	public static implicit operator (TId parent, TId child)(Relation<TId> relation)
 	{
 		return (relation.Parent, relation.Child);
