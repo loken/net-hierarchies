676171b5f151a14601022362b213d23996463eea
Gaute Løken <gautelo@gmail.com>
2023-04-05 03:16:12 +0200

    Added a test asserting that json serialization works correctly for nodes.

---
+++ Hierarchies.Tests/NodeJsonSerializationTests.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Loken.Hierarchies;
+public class NodeJsonSerializationTests
+{
+	[Fact]
+	public void Stuff()
+	{
+		const string expectedJson = """
+		{
+		  "Children": [
+		    {
+		      "Children": null,
+		      "Value": "a"
+		    }
+		  ],
+		  "Value": "root"
+		}
+		""";
+
+		var root = Node.Create("root").Attach(Node.Create("a"));
+		var nodes = root.GetDescendants(true).ToArray();
+
+		var actualJson = JsonConvert.SerializeObject(root, Formatting.Indented);
+		Assert.Equal(expectedJson, actualJson);
+
+		var deserializedRoot = JsonConvert.DeserializeObject<Node<string>>(actualJson)!;
+		var deserializedNodes = deserializedRoot.GetDescendants(true).ToArray();
+		Assert.Equal(nodes, deserializedNodes);
+	}
+}
