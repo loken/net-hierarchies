2ab8d64659423de4e4628fa8cee9f0f5680055ed
Gaute Løken <gautelo@gmail.com>
2023-04-16 17:37:25 +0200

    Added convenience extensions for attaching/detaching an enumerable of nodes.

---
+++ Hierarchies/HierarchyLinkingExtensions.cs
@@ -0,0 +1,60 @@
+﻿namespace Loken.Hierarchies;
+
+/// <summary>
+/// Extensions for managing how the <see cref="Node{TItem}"/>s of a <see cref="Hierarchy{TItem, TId}"/> are linked,
+/// meaning their parent-child-relations.
+/// </summary>
+public static class HierarchyLinkingExtensions
+{
+	/// <summary>
+	/// Attach the provided <paramref name="roots"/> to the <paramref name="hierarchy"/>.
+	/// </summary>
+	/// <param name="hierarchy">Hierarchy to attach to.</param>
+	/// <param name="roots">Nodes to attach.</param>
+	/// <returns>The hierarchy itself for method chaining purposes.</returns>
+	/// <exception cref="InvalidOperationException">
+	/// Must provide non-empty set of <paramref name="roots"/> of a compatible brand
+	/// that aren't already attached to another parent.
+	/// </exception>
+	public static Hierarchy<TItem, TId> Attach<TItem, TId>(this Hierarchy<TItem, TId> hierarchy, IEnumerable<Node<TItem>> roots)
+		where TItem : notnull
+		where TId : notnull
+	{
+		return hierarchy.Attach(roots.ToArray());
+	}
+
+	/// <summary>
+	/// Attach the provided <paramref name="children"/> to the node of the
+	/// provided <paramref name="parentId"/> which is in the <paramref name="hierarchy"/>.
+	/// </summary>
+	/// <param name="hierarchy">Hierarchy to attach to.</param>
+	/// <param name="parentId">The id of the node to attach to.</param>
+	/// <param name="children">Nodes to attach.</param>
+	/// <returns>The hierarchy itself for method chaining purposes.</returns>
+	/// <exception cref="InvalidOperationException">
+	/// Must provide non-empty set of <paramref name="children"/> of a compatible brand
+	/// that aren't already attached to another parent.
+	/// </exception>
+	public static Hierarchy<TItem, TId> Attach<TItem, TId>(this Hierarchy<TItem, TId> hierarchy, TId parentId, IEnumerable<Node<TItem>> children)
+		where TItem : notnull
+		where TId : notnull
+	{
+		return hierarchy.Attach(parentId, children.ToArray());
+	}
+
+	/// <summary>
+	/// Detach the provided <paramref name="nodes"/>.
+	/// </summary>
+	/// <param name="hierarchy">Hierarchy to detach from.</param>
+	/// <param name="nodes">Nodes to detach.</param>
+	/// <returns>The hierarchy itself for method chaining purposes.</returns>
+	/// <exception cref="InvalidOperationException">
+	/// Must provide non-empty set of attached <paramref name="nodes"/>.
+	/// </exception>
+	public static Hierarchy<TItem, TId> Detach<TItem, TId>(this Hierarchy<TItem, TId> hierarchy, IEnumerable<Node<TItem>> nodes)
+		where TItem : notnull
+		where TId : notnull
+	{
+		return hierarchy.Detach(nodes.ToArray());
+	}
+}
--- Hierarchies/NodeLinkingExtensions.cs
+++ Hierarchies/NodeLinkingExtensions.cs
@@ -1,7 +1,42 @@
 ﻿namespace Loken.Hierarchies;
 
+/// <summary>
+/// Extensions for managing how <see cref="Node{TItem}"/>s are linked,
+/// meaning their parent-child-relations.
+/// </summary>
 public static class NodeLinkingExtensions
 {
+	/// <summary>
+	/// Attach the provided <paramref name="children"/> to the <paramref name="parent"/>.
+	/// </summary>
+	/// <param name="parent">Node to attach to.</param>
+	/// <param name="children">Nodes to attach.</param>
+	/// <returns>The <paramref name="parent"/> for method chaining purposes.</returns>
+	/// <exception cref="InvalidOperationException">
+	/// Must provide non-empty set of <paramref name="children"/> of a compatible brand
+	/// that aren't already attached to another parent.
+	/// </exception>
+	public static Node<TItem> Attach<TItem>(this Node<TItem> parent, IEnumerable<Node<TItem>> children)
+		where TItem : notnull
+	{
+		return parent.Attach(children.ToArray());
+	}
+
+	/// <summary>
+	/// Detach the provided <paramref name="children"/> from the <paramref name="parent"/>.
+	/// </summary>
+	/// <param name="parent">Node to detach from.</param>
+	/// <param name="children">Nodes to detach.</param>
+	/// <returns>The <paramref name="parent"/> for method chaining purposes.</returns>
+	/// <exception cref="InvalidOperationException">
+	/// Must provide non-empty set of attached and non-branded <paramref name="children"/>.
+	/// </exception>
+	public static Node<TItem> Detach<TItem>(this Node<TItem> parent, IEnumerable<Node<TItem>> children)
+		where TItem : notnull
+	{
+		return parent.Detach(children.ToArray());
+	}
+
 	/// <summary>
 	/// Disassmbling a node means to cascade detach it.
 	/// We always caschade detach the nodes.
