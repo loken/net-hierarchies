4b2e55a35a3d501a1f38b12cab13f05c067f9667
Gaute Løken <gautelo@gmail.com>
2023-04-16 03:33:10 +0200

    Added Node.Assemble(..) methods for creating nodes that are assembled or linked according to the provided childMap.

--- Hierarchies.Tests/Item.cs
+++ Hierarchies.Tests/Item.cs
@@ -5,7 +5,7 @@ namespace Loken.Hierarchies;
 [DebuggerDisplay("Id: {" + nameof(Id) + "}")]
 internal class Item<TId>
 {
-	public TId Id { get; set; }
+	public TId Id { get; private set; }
 
 	public Item(TId id)
 	{
--- Hierarchies/Node.cs
+++ Hierarchies/Node.cs
@@ -1,4 +1,6 @@
-﻿namespace Loken.Hierarchies;
+﻿using Loken.System.Collections;
+
+namespace Loken.Hierarchies;
 
 /// <summary>
 /// Factory class for <see cref="Node{TItem}"/> which allow us to infer
@@ -31,5 +33,81 @@ public static class Node
 		where TItem : notnull
 	{
 		return CreateMany(items.AsEnumerable()).ToArray();
+	}
+
+	/// <summary>
+	/// Assemble nodes of <paramref name="items"/> linked as described by the provided <paramref name="childMap"/>.
+	/// </summary>
+	/// <typeparam name="TItem">The type of item.</typeparam>
+	/// <typeparam name="TId">The type of IDs.</typeparam>
+	/// <param name="identify">Means of getting an ID for an item.</param>
+	/// <param name="items">The items to wrap in nodes.</param>
+	/// <param name="childMap">The map describing the relations.</param>
+	/// <returns>The root nodes.</returns>
+	public static ICollection<Node<TItem>> Assemble<TItem, TId>(Func<TItem, TId> identify, IEnumerable<TItem> items, IDictionary<TId, ISet<TId>> childMap)
+		where TItem : notnull
+		where TId : notnull
+	{
+		var nodes = new Dictionary<TId, Node<TItem>>();
+		var roots = new Dictionary<TId, Node<TItem>>();
+
+		foreach (var item in items)
+		{
+			var id = identify(item);
+			var node = Create(item);
+
+			nodes.Add(id, node);
+
+			if (childMap.ContainsKey(id))
+				roots.Add(id, node);
+		}
+
+		foreach (var (parentId, childIds) in childMap)
+		{
+			var parent = nodes[parentId];
+
+			foreach (var childId in childIds)
+			{
+				var childNode = nodes[childId];
+				parent.Attach(childNode);
+				roots.Remove(childId);
+			}
+		}
+
+		return roots.Values;
+	}
+
+	/// <summary>
+	/// Assemble nodes of IDs linked as described by the provided <paramref name="childMap"/>.
+	/// </summary>
+	/// <typeparam name="TId">The type of IDs.</typeparam>
+	/// <param name="childMap">The map describing the relations.</param>
+	/// <returns>The root nodes.</returns>
+	public static ICollection<Node<TId>> Assemble<TId>(IDictionary<TId, ISet<TId>> childMap)
+		where TId : notnull
+	{
+		var nodes = new Dictionary<TId, Node<TId>>();
+		var roots = new Dictionary<TId, Node<TId>>();
+
+		foreach (var parentId in childMap.Keys)
+		{
+			var parentNode = Create(parentId);
+			roots.Add(parentId, parentNode);
+			nodes.Add(parentId, parentNode);
+		}
+
+		foreach (var (parentId, childIds) in childMap)
+		{
+			var parentNode = nodes[parentId];
+
+			foreach (var childId in childIds)
+			{
+				var childNode = nodes.Lazy(childId, () => Create(childId));
+				parentNode.Attach(childNode);
+				roots.Remove(childId);
+			}
+		}
+
+		return roots.Values;
 	}
 }
