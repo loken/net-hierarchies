b1d5d8a2a22db26996026e82a750370bf85c81f3
Gaute Løken <gautelo@gmail.com>
2023-05-06 18:00:05 +0200

    Added some tests for managing HierarchyRelations in mongodb.

---
+++ Hierarchies.MongoDB.Tests/CreationTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Loken.Hierarchies.Data.MongoDB;
+
+[Collection("MongoDB")]
+public class CreationTests : IClassFixture<DbFixture>
+{
+	private readonly DbFixture Fixture;
+
+	public CreationTests(DbFixture fixture)
+	{
+		Fixture = fixture;
+	}
+
+	[Fact]
+	public void CreateRelationIndexes()
+	{
+		var collection = Fixture.GetDatabase().GetHierarchies<string>().CreateRelationIndexes();
+
+		var indexNames = collection.Indexes.List()
+			.ToEnumerable()
+			.Select(d => d.GetValue("name").AsString)
+			.ToArray();
+
+		Assert.Equivalent("_id_|Id_Concept_Type|Concept_Type_Id|Type_Concept_Id".Split('|'), indexNames);
+	}
+}
---
+++ Hierarchies.MongoDB.Tests/QueryTests.cs
@@ -0,0 +1,54 @@
+﻿using Loken.System.Collections.MultiMap;
+
+namespace Loken.Hierarchies.Data.MongoDB;
+
+[Collection("MongoDB")]
+public class QueryTests : IClassFixture<DbFixture>
+{
+	private readonly DbFixture Fixture;
+
+	private const string ChildMap = """
+		A:A1,A2
+		A1:A11,A12
+		B:B1
+		B1:B12
+		""";
+
+	public QueryTests(DbFixture fixture)
+	{
+		Fixture = fixture;
+	}
+
+	[Fact]
+	public void GetRelations()
+	{
+		var collection = Fixture.GetDatabase().GetHierarchies<string>().CreateRelationIndexes();
+		var childMap = ChildMap.ParseMultiMap();
+		var hc = Hierarchy.CreateMapped(childMap);
+		collection.InsertRelations(hc, "companies");
+
+		var descendantMap = hc.ToMap(RelType.Descendants);
+		var ancestorMap = hc.ToMap(RelType.Ancestors);
+
+		var readChildMap = collection.GetRelations<string>("companies", RelType.Children).ToMap();
+		var readDescendantsMap = collection.GetRelations<string>("companies", RelType.Descendants).ToMap();
+		var readAncestorsMap = collection.GetRelations<string>("companies", RelType.Ancestors).ToMap();
+
+		Assert.Equivalent(childMap, readChildMap);
+		Assert.Equivalent(descendantMap, readDescendantsMap);
+		Assert.Equivalent(ancestorMap, readAncestorsMap);
+	}
+
+	[Fact]
+	public void GetChildren()
+	{
+		var collection = Fixture.GetDatabase().GetHierarchies<string>().CreateRelationIndexes();
+		var childMap = ChildMap.ParseMultiMap();
+		var hc = Hierarchy.CreateMapped(childMap);
+		collection.InsertRelations(hc, "companies", RelType.Children);
+
+		var childrenOfA = collection.GetChildren("companies", "A");
+
+		Assert.Equivalent(childMap["A"], childrenOfA);
+	}
+}
