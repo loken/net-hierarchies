b9e5ad3dd79c35c6d2b0ea34c1593e0d85e93ebb
Gaute Løken <gautelo@gmail.com>
2023-05-06 17:08:40 +0200

    Added a MongoDB.Tests project along with a docker-compose.yml for hosting a mongodb image and starting the image from github actions.

---
+++ Hierarchies.MongoDB.Tests/Usings.cs
@@ -0,0 +1,6 @@
+global using Loken.Hierarchies;
+global using Loken.Hierarchies.Data;
+global using Loken.Hierarchies.Data.MongoDB;
+global using Loken.Hierarchies.Traversal;
+global using MongoDB.Driver;
+global using Xunit;
