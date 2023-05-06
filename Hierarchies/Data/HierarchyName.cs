e226235e3073987427d071a613be2523f1d0340a
Gaute Løken <gautelo@gmail.com>
2023-05-06 16:14:38 +0200

    Removed the HierarchyTable with a HierarchyName. We are going to change our approach so that more HierarchyRelations will be put in the same table/collection, so we don't need as many variants.

---
+++ Hierarchies/Data/HierarchyName.cs
@@ -0,0 +1,16 @@
+﻿namespace Loken.Hierarchies.Data;
+
+/// <summary>
+/// Knows how to create table/collection names for holding <see cref="HierarchyRelation{TId}"/>s.
+/// </summary>
+/// <remarks>
+/// The methods take a generic type parameter for the type of IDs
+/// so that we don't mix relationships of different types of IDs.
+/// </remarks>
+public static class HierarchyName
+{
+	/// <summary>
+	/// Create a table/collection name for holding node-to-children relations.
+	/// </summary>
+	public static string For<TId>() where TId : notnull => $"Loken_Hierarchies_{typeof(TId).Name}";
+}
