15c78742778f96bf6c0eb275ddb4db6f4a8e294f
Gaute Løken <gautelo@gmail.com>
2023-05-06 17:18:46 +0200

    Added a DbFixture for the mongodb tests which reads environment variables from a .env file and provides a way to dynamically create and drop mongodb databases.

---
+++ Hierarchies.MongoDB.Tests/DbFixture.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Loken.Hierarchies.Data.MongoDB;
+
+public class DbFixture : IDisposable
+{
+	static DbFixture()
+	{
+		DotEnv.Load();
+
+		var config = new ConfigurationBuilder()
+			.AddEnvironmentVariables()
+			.Build();
+
+		var username = config.GetSection("MONGO_INITDB_ROOT_USERNAME").Value;
+		var password = config.GetSection("MONGO_INITDB_ROOT_PASSWORD").Value;
+		var connectionString = $"mongodb://{username}:{password}@localhost:27017";
+
+		var collection = new ServiceCollection();
+		collection.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+
+		collection.BuildServiceProvider();
+
+		var services = collection.BuildServiceProvider();
+
+		HierarchyMongo.RegisterConventions();
+		Client = services.GetRequiredService<IMongoClient>();
+	}
+
+	public DbFixture()
+	{
+	}
+
+	protected ISet<string> DatabaseNames { get; } = new HashSet<string>();
+
+	protected static IMongoClient Client { get; }
+
+	public IMongoDatabase GetDatabase(string? name = default)
+	{
+		name ??= $"test_{Guid.NewGuid()}";
+		DatabaseNames.Add(name);
+		return Client.GetDatabase(name);
+	}
+
+	public void Dispose()
+	{
+		foreach (var dbName in DatabaseNames)
+			Client.DropDatabase(dbName);
+	}
+}
