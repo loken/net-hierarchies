4e858bc3684ec4a394b20b97ceffa0d8eb8b1688
Gaute Løken <gautelo@gmail.com>
2025-08-10 17:18:33 +0200

    #sync #find Added some missing extension methods for common ancestors needed for feature parity with TS. Rewrote tests to match TS as well.

--- Hierarchies.Tests/FindCommonAncestorExtensionsTests.cs
+++ Hierarchies.Tests/FindCommonAncestorExtensionsTests.cs
@@ -2,118 +2,147 @@ namespace Loken.Hierarchies;
 
 public class FindCommonAncestorExtensionsTests
 {
-	// Create explicit hierarchy for testing:
+	// Create hierarchy of individually attached nodes so we have references.
 	// A -> A1 -> A11
 	//   -> A2 -> A12
 	// B -> B1 -> B12
 	// C
+	private static readonly Node<string> NodeRoot = Nodes.Create("Root");
 	private static readonly Node<string> NodeA = Nodes.Create("A");
+	private static readonly Node<string> NodeB = Nodes.Create("B");
 	private static readonly Node<string> NodeA1 = Nodes.Create("A1");
 	private static readonly Node<string> NodeA2 = Nodes.Create("A2");
 	private static readonly Node<string> NodeA11 = Nodes.Create("A11");
 	private static readonly Node<string> NodeA12 = Nodes.Create("A12");
-	private static readonly Node<string> NodeB = Nodes.Create("B");
+	private static readonly Node<string> NodeA21 = Nodes.Create("A21");
+	private static readonly Node<string> NodeA22 = Nodes.Create("A22");
 	private static readonly Node<string> NodeB1 = Nodes.Create("B1");
-	private static readonly Node<string> NodeB12 = Nodes.Create("B12");
-	private static readonly Node<string> NodeC = Nodes.Create("C");
-
-	private static readonly Node<string>[] Roots = { NodeA, NodeB, NodeC };
+	private static readonly Node<string> NodeB2 = Nodes.Create("B2");
+	private static readonly Node<string> NodeB11 = Nodes.Create("B11");
 
 	static FindCommonAncestorExtensionsTests()
 	{
 		// Build the hierarchy
+		NodeRoot.Attach(NodeA, NodeB);
 		NodeA.Attach(NodeA1, NodeA2);
 		NodeA1.Attach(NodeA11, NodeA12);
-		NodeB.Attach(NodeB1);
-		NodeB1.Attach(NodeB12);
+		NodeA2.Attach(NodeA21, NodeA22);
+		NodeB.Attach(NodeB1, NodeB2);
+		NodeB1.Attach(NodeB11);
 	}
 
 	#region FindCommonAncestor Tests
-
 	[Fact]
-	public void FindCommonAncestor_ReturnsTheClosestCommonAncestor()
+	public void FindCommonAncestor_FindsClosestCommonAncestor()
 	{
-		var actual = new[] { NodeA11, NodeA2 }.FindCommonAncestor();
-		Assert.Equal(NodeA, actual);
+		// A11 and A12 should have A1 as common ancestor
+		var actual = new[] { NodeA11, NodeA12 }.FindCommonAncestor();
+		Assert.Equal(NodeA1, actual);
+
+		// A11 and A21 should have A as common ancestor
+		var actual2 = new[] { NodeA11, NodeA21 }.FindCommonAncestor();
+		Assert.Equal(NodeA, actual2);
+
+		// A11 and B11 should have Root as common ancestor
+		var actual3 = new[] { NodeA11, NodeB11 }.FindCommonAncestor();
+		Assert.Equal(NodeRoot, actual3);
 	}
 
 	[Fact]
-	public void FindCommonAncestor_ReturnsNullWhenThereIsNoCommonAncestor()
+	public void FindCommonAncestor_WithSingleNodeReturnsFirstAncestor()
 	{
-		var actual = new[] { NodeA1, NodeB1 }.FindCommonAncestor();
+		// A11's first ancestor should be A1
+		var actual = new[] { NodeA11 }.FindCommonAncestor();
+		Assert.Equal(NodeA1, actual);
+	}
+
+	[Fact]
+	public void FindCommonAncestor_WithSingleRootNodeReturnsNull()
+	{
+		// Root has no ancestors
+		var actual = new[] { NodeRoot }.FindCommonAncestor();
 		Assert.Null(actual);
 	}
 
 	[Fact]
-	public void FindCommonAncestor_WorksWithIncludeSelfTrue()
+	public void FindCommonAncestor_WithNonExistentNodesReturnsNull()
 	{
-		var actual = new[] { NodeA1, NodeA2 }.FindCommonAncestor(true);
-		Assert.Equal(NodeA, actual);
+		var nonExistent1 = Nodes.Create("NonExistent1");
+		var nonExistent2 = Nodes.Create("NonExistent2");
+		var actual = new[] { nonExistent1, nonExistent2 }.FindCommonAncestor();
+		Assert.Null(actual);
 	}
 
 	[Fact]
-	public void FindCommonAncestor_ReturnsTheNodeItselfWhenSingleNodePassed()
+	public void FindCommonAncestor_WithIncludeSelfFindsAncestorIncludingTargetNodes()
 	{
-		var actual = NodeA11.FindCommonAncestor(true);
-		Assert.Equal(NodeA11, actual);
+		// A1 and A11 with includeSelf should return A1 (since A1 is ancestor of A11 and includeSelf includes A1)
+		var actual = new[] { NodeA1, NodeA11 }.FindCommonAncestor(true);
+		Assert.Equal(NodeA1, actual);
 	}
-
 	#endregion
 
 	#region FindCommonAncestors Tests
-
 	[Fact]
 	public void FindCommonAncestors_ReturnsAllCommonAncestors()
 	{
 		var actual = new[] { NodeA11, NodeA12 }.FindCommonAncestors();
-		var items = actual.Select(n => n.Item).OrderBy(x => x).ToArray();
-		Assert.Equal(new[] { "A", "A1" }, items);
+		Assert.Equal([NodeA1, NodeA, NodeRoot], actual);
 	}
 
 	[Fact]
-	public void FindCommonAncestors_ReturnsEmptyListWhenThereAreNoCommonAncestors()
+	public void FindCommonAncestors_ReturnsAllAncestorsForSingleNode()
 	{
-		var actual = new[] { NodeA1, NodeB1 }.FindCommonAncestors();
+		var actual = new[] { NodeA11 }.FindCommonAncestors();
+		Assert.Equal([NodeA1, NodeA, NodeRoot], actual);
+	}
+
+	[Fact]
+	public void FindCommonAncestors_ReturnsEmptyForSingleRoot()
+	{
+		var actual = new[] { NodeRoot }.FindCommonAncestors();
 		Assert.Empty(actual);
 	}
 
-	#endregion
-
-	#region FindCommonAncestorItems Tests
-
 	[Fact]
-	public void FindCommonAncestorItems_ReturnsCommonAncestorItems()
+	public void FindCommonAncestors_WithIncludeSelfReturnsQueriedRootItem()
 	{
-		var actual = new[] { NodeA11, NodeA12 }.FindCommonAncestorItems();
-		var items = actual.OrderBy(x => x).ToArray();
-		Assert.Equal(new[] { "A", "A1" }, items);
+		var actual = new[] { NodeRoot }.FindCommonAncestors(true);
+		Assert.Equal([NodeRoot], actual);
 	}
 
 	[Fact]
-	public void FindCommonAncestorItems_ReturnsEmptyListWhenThereAreNoCommonAncestors()
+	public void FindCommonAncestors_WithUnrelatedBranchesReturnsRoot()
 	{
-		var actual = new[] { NodeA1, NodeB1 }.FindCommonAncestorItems();
+		var actual = new[] { NodeA11, NodeB11 }.FindCommonAncestors();
+		Assert.Equal([NodeRoot], actual);
+	}
+
+	[Fact]
+	public void FindCommonAncestors_WithNoCommonAncestorsReturnsEmpty()
+	{
+		var nonExistent1 = Nodes.Create("NonExistent1");
+		var nonExistent2 = Nodes.Create("NonExistent2");
+		var actual = new[] { nonExistent1, nonExistent2 }.FindCommonAncestors();
 		Assert.Empty(actual);
 	}
-
 	#endregion
 
 	#region FindCommonAncestorSet Tests
-
 	[Fact]
 	public void FindCommonAncestorSet_ReturnsSetOfCommonAncestors()
 	{
 		var actual = new[] { NodeA11, NodeA12 }.FindCommonAncestorSet();
-		var items = actual!.Select(n => n.Item).OrderBy(x => x).ToArray();
-		Assert.Equal(new[] { "A", "A1" }, items);
+		Assert.Equal([ NodeA1, NodeA, NodeRoot ], actual);
 	}
 
 	[Fact]
 	public void FindCommonAncestorSet_ReturnsEmptySetWhenThereAreNoCommonAncestors()
 	{
-		var actual = new[] { NodeA1, NodeB1 }.FindCommonAncestorSet();
+		var nonExistent1 = Nodes.Create("NonExistent1");
+		var nonExistent2 = Nodes.Create("NonExistent2");
+		var actual = new[] { nonExistent1, nonExistent2 }.FindCommonAncestorSet();
 		Assert.Empty(actual!);
 	}
-
 	#endregion
 }
